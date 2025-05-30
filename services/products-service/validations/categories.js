a489daa60f01ed87fbddd822f77029c2b730439a
KuroiNekOo <maximegomes.dinis@outlook.fr>
2025-05-30 14:17:41 +0200

    feat: :sparkles: schemas for get by id ( products / categories )

--- services/products-service/validations/categories.js
+++ services/products-service/validations/categories.js
@@ -1,16 +1,20 @@
 import {  z } from 'zod';
 
+export const getCategorySchema = z.object({
+  id: z.coerce.number().positive('L\'id doit être un nombre positif'),
+});
+
 export const createCategorySchema = z.object({
-  name: z.string().min(1, 'Name is required').required(),
+  name: z.string().min(1, 'Name is required'),
   description: z.string().min(1, 'Description is required').optional(),
 });
 
 export const updateCategorySchema = z.object({
-  id: z.number().positive().required(),
+  id: z.number().positive('L\'id doit être un nombre positif'),
   name: z.string().min(1, 'Name is required').optional(),
   description: z.string().min(1, 'Description is required').optional(),
 });
 
 export const deleteCategorySchema = z.object({
-  id: z.number().positive().required(),
+  id: z.coerce.number().positive('L\'id doit être un nombre positif'),
 });
--- services/products-service/validations/products.js
+++ services/products-service/validations/products.js
@@ -1,20 +1,24 @@
 import { z } from 'zod';
 
+export const getProductSchema = z.object({
+  id: z.coerce.number().positive('L\'id doit être un nombre positif'),
+});
+
 export const createProductSchema = z.object({
-  name: z.string().min(1, 'Name is required').required(),
+  name: z.string().min(1, 'Name is required'),
   description: z.string().min(1, 'Description is required').optional(),
-  price: z.number().min(0, 'Price must be a positive number').required(),
-  category: z.number().positive().optional(),
+  price: z.number().min(0, 'Price must be a positive number'),
+  categoryId: z.number().positive().optional(),
 });
 
 export const updateProductSchema = z.object({
-  id: z.number().positive().required(),
+  id: z.number().positive('L\'id doit être un nombre positif'),
   name: z.string().min(1, 'Name is required').optional(),
   description: z.string().min(1, 'Description is required').optional(),
   price: z.number().min(0, 'Price must be a positive number').optional(),
-  category: z.number().positive().optional(),
+  categoryId: z.number().positive().optional(),
 });
 
 export const deleteProductSchema = z.object({
-  id: z.number().positive().required(),
+  id: z.coerce.number().positive('L\'id doit être un nombre positif'),
 });
