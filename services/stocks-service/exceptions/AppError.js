020d30cc7393ec33d9c4639e10fc68193baae0ef
anass raiss <anassraiss165@gmail.com>
2025-06-07 22:21:07 +0200

    feat: :sparkles: custom exceptions for errors handler

---
+++ services/stocks-service/exceptions/AppError.js
@@ -0,0 +1,8 @@
+class AppError extends Error {
+    constructor(statusCode, error, message) {
+        super(message);
+        this.statusCode = statusCode;
+        this.error = error;
+    }
+}
+export default AppError;
