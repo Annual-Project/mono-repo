a304c025e5cc49a51a1abc0b5c698da3a9d5196d
KuroiNekOo <maximegomes.dinis@outlook.fr>
2025-06-20 14:51:02 +0200

    feat: :sparkles: init web server with express and node http for auth service

---
+++ services/auth-service/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+
+import errorMiddleware from './middlewares/errors.js';
+import router from './routes/index.js';
+import authMiddleware from "./middlewares/auth.js";
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'development'
+    ? '*'
+    : 'https://skillforge.keyce.fr',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(authMiddleware);
+
+app.use(router);
+
+app.use(errorMiddleware);
+
+export default app;
