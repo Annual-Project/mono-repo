bbc9c46d7353d4b761293bc8ded810d3b1616a97
KuroiNekOo <maximegomes.dinis@outlook.fr>
2025-07-01 14:42:10 +0200

    feat: :sparkles: validations middleware for notifications service

---
+++ services/notifications-service/middlewares/validations.js
@@ -0,0 +1,32 @@
+/**
+ * Middleware de validation des données avec Zod
+ * @param {Object} schema - Le schéma Zod à utiliser pour la validation
+ * @param {string} source - La source des données à valider ('params', 'body', 'query')
+ */
+const validateData = (schema, source) => {
+  return (req, _, next) => {
+    try {
+      // Vérifie si la source est valide
+      if (!['params', 'body', 'query'].includes(source)) {
+        throw new Error(`Source invalide pour la validation: ${source}`);
+      }
+
+      // Valide les données en fonction de la source
+      const data = req[source];
+
+      console.log(data);
+
+      // Lance une erreur si les données sont manquantes (ZodError)
+      const validatedData = schema.parse(data);
+
+      // Ajoute les données validées à la requête
+      req[source] = validatedData;
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default validateData;
