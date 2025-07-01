d5db96865d94994d9f7c74ee953da394a71d20cb
KuroiNekOo <maximegomes.dinis@outlook.fr>
2025-07-01 20:10:13 +0200

    feat: :sparkles: add safeStringEmail function for zod validation

--- shared/validations/communs.js
+++ shared/validations/communs.js
@@ -2,4 +2,14 @@ import { z } from 'zod';
 import xss from 'xss';
 
 // Schéma de base pour une chaîne nettoyée
-export const safeString = z.string().trim().transform((val) => xss(val));
+export const safeString = (minLength = 1) =>
+  z.string()
+    .trim()
+    .min(minLength, `La chaîne doit contenir au moins ${minLength} caractère(s)`)
+    .transform((val) => xss(val));
+
+export const safeStringEmail = (minLength = 1) =>
+  z.string()
+    .trim()
+    .email("L'email doit être valide")
+    .transform((val) => xss(val));
